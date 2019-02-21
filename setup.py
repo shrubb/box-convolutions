fe271adbc7f7c5f3e40b53a92e0970f15b40b14b
Egor Burkov <shrubb@ya.ru>
2019-02-21 17:42:50 +0300

    Enable CPU-only builds (#10)

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-from setuptools import setup
-import torch.utils.cpp_extension
-
 # monkey-patch for parallel compilation
 # https://stackoverflow.com/questions/11013851/speeding-up-build-process-with-distutils
 def parallelCCompile(
@@ -34,14 +31,17 @@ import distutils.ccompiler
 distutils.ccompiler.CCompiler.compile = parallelCCompile
 
 import os
+import torch
+
+build_cuda = torch.cuda.is_available() # TODO allow cross-compiling too
 
 source_root = 'src'
 source_files_cpp = [
-	'integral_image_interface.cpp',
-	'integral_image.cpp',
-	'box_convolution_interface.cpp',
-	'box_convolution.cpp',
-	'bind.cpp'
+    'integral_image_interface.cpp',
+    'integral_image.cpp',
+    'box_convolution_interface.cpp',
+    'box_convolution.cpp',
+    'bind.cpp'
 ]
 source_files_cuda = [
     'integral_image_cuda.cu',
@@ -49,13 +49,20 @@ source_files_cuda = [
     'box_convolution_cuda_backward.cu',
     'box_convolution_cuda_misc.cu'
 ]
-source_files = source_files_cpp + source_files_cuda
+source_files_cuda_stubs = [
+    'cuda_stubs.cpp'
+]
+source_files = source_files_cpp + (source_files_cuda if build_cuda else source_files_cuda_stubs)
 
-cpp_cuda = torch.utils.cpp_extension.CUDAExtension(
+from torch.utils.cpp_extension import CppExtension, CUDAExtension
+
+cpp_cuda = (CUDAExtension if build_cuda else CppExtension)(
     name='box_convolution_cpp_cuda',
     sources=[os.path.join(source_root, file) for file in source_files],
     include_dirs=[source_root]
 )
+
+from setuptools import setup
 
 setup(
     name='box_convolution',
---
+++ src/cuda_stubs.cpp
@@ -0,0 +1,115 @@
+#include <torch/extension.h>
+
+#include "box_convolution.h" // for `enum class Parameter`
+
+#define STUB_ERROR AT_ERROR("box_convolution was compiled withoud CUDA support because " \
+                            "torch.cuda.is_available() was False when you ran setup.py.")
+
+namespace gpu {
+
+void integral_image(at::Tensor & input, at::Tensor & output)
+{ STUB_ERROR; }
+
+void splitParameters(
+    at::Tensor & x_min   , at::Tensor & x_max   , at::Tensor & y_min   , at::Tensor & y_max   ,
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac)
+{ STUB_ERROR; }
+
+void splitParametersUpdateGradInput(
+    at::Tensor & x_min   , at::Tensor & x_max   , at::Tensor & y_min   , at::Tensor & y_max   ,
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac)
+{ STUB_ERROR; }
+
+void splitParametersAccGradParameters(
+    at::Tensor & x_min   , at::Tensor & x_max   , at::Tensor & y_min   , at::Tensor & y_max   ,
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac)
+{ STUB_ERROR; }
+
+template <bool normalize, bool exact>
+void boxConvUpdateOutput(
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac,
+    at::Tensor & area, at::Tensor & input_integrated, at::Tensor & output)
+{ STUB_ERROR; }
+
+// explicitly instantiate
+template void boxConvUpdateOutput<true, true>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateOutput<false, true>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateOutput<true, false>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateOutput<false, false>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template <bool normalize, bool exact>
+void boxConvUpdateGradInput(
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac,
+    at::Tensor & area, at::Tensor & grad_output_integrated, at::Tensor & tmpArray)
+{ STUB_ERROR; }
+
+// explicitly instantiate
+template void boxConvUpdateGradInput<true, true>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateGradInput<false, true>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateGradInput<true, false>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template void boxConvUpdateGradInput<false, false>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &);
+
+template <bool exact>
+void boxConvAccGradParameters(
+    at::Tensor & xMinInt , at::Tensor & xMaxInt , at::Tensor & yMinInt , at::Tensor & yMaxInt ,
+    at::Tensor & xMinFrac, at::Tensor & xMaxFrac, at::Tensor & yMinFrac, at::Tensor & yMaxFrac,
+    at::Tensor & input_integrated, at::Tensor & tmpArray, Parameter parameter)
+{ STUB_ERROR; }
+
+// explicitly instantiate
+template void boxConvAccGradParameters<true>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, Parameter);
+
+template void boxConvAccGradParameters<false>(
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, at::Tensor &, at::Tensor &,
+    at::Tensor &, at::Tensor &, Parameter);
+
+void clipParameters(
+    at::Tensor & paramMin, at::Tensor & paramMax,
+    const double reparametrization, const double minSize, const double maxSize)
+{ STUB_ERROR; }
+
+at::Tensor computeArea(
+    at::Tensor x_min, at::Tensor x_max, at::Tensor y_min, at::Tensor y_max,
+    const bool exact, const bool needXDeriv, const bool needYDeriv)
+{ STUB_ERROR; }
+
+}
